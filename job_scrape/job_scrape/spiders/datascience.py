15c7873fb4be771d872ac7546ddd9da7fac91462
HeavyJab <lamming0912@gmail.com>
2018-12-31 00:18:30 +1100

    Scraping data science job ads

--- job_scrape/job_scrape/spiders/datascience.py
+++ job_scrape/job_scrape/spiders/datascience.py
@@ -4,8 +4,14 @@ import scrapy
 
 class DatascienceSpider(scrapy.Spider):
     name = 'datascience'
-    start_urls = ['https://www.seek.com.au/data-scientist-jobs']
+    start_urls = ['https://www.seek.com.au/data-scientist-jobs/in-All-Melbourne-VIC']
 
     def parse(self, response):
-        ads = response.xpath('//div[@data-sol-meta]').extract()
-        yield {'ads': ads}
+        for url in response.xpath('//div[@data-sol-meta]/article/span/div[@href]/@href').extract():
+            url = response.urljoin(url)
+            print(url)
+            yield scrapy.Request(url=url, callback=self.parse)
+
+        ads = {}
+        ads['job_date'] = response.xpath('//section[@aria-labelledby="jobInfoHeader"]//text()').extract()
+        yield ads
--- scraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = ('https://newsapi.org/v2/top-headlines?'
-        'everything?'
-       'country=au&'
-       'q=house&'
-       'apiKey=2596cf07104c4dc489e7a5415c7c9c23')
-response = requests.get(url)
-print(response.json())
