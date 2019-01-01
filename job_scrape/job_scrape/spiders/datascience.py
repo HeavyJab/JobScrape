38b558d0d2d40c4cb4f3c0209682329ee7b72b7b
HeavyJab <lamming0912@gmail.com>
2019-01-02 02:08:40 +1100

    - Added the following attributes to output file: Job Title Recruiter Job Date Work Type Job Description
    - Added lexicon for job description parsing

--- job_scrape/job_scrape/spiders/datascience.py
+++ job_scrape/job_scrape/spiders/datascience.py
@@ -9,9 +9,13 @@ class DatascienceSpider(scrapy.Spider):
     def parse(self, response):
         for url in response.xpath('//div[@data-sol-meta]/article/span/div[@href]/@href').extract():
             url = response.urljoin(url)
-            print(url)
             yield scrapy.Request(url=url, callback=self.parse)
 
         ads = {}
-        ads['job_date'] = response.xpath('//section[@aria-labelledby="jobInfoHeader"]//text()').extract()
+        header = response.xpath('//section[@aria-labelledby="jobInfoHeader"]//text()').extract()
+        ads['job_title'] = response.xpath('//span[@data-automation="job-detail-title"]//text()').extract_first()
+        ads['recruiter'] = response.xpath('//span[@data-automation="advertiser-name"]//text()').extract_first()
+        ads['job_date'] =  response.xpath('//dd[@data-automation="job-detail-date"]//text()').extract_first()
+        ads['work_type'] = response.xpath('//dd[@data-automation="job-detail-work-type"]//text()').extract_first()
+        ads['job_desc'] = response.xpath('//div[@data-automation="mobileTemplate"]//div//text()|ul//text()').extract_first()
         yield ads
